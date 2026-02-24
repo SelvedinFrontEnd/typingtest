30e3f6635e3609fc875b31fa8262629ef7856a13
Selvedin Drinic <121358456+SelvedinFrontEnd@users.noreply.github.com>
2026-02-24 22:47:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,7 @@ let correct;
 let testSection = document.getElementById("test-section")
 let testCompleted = document.getElementById("results")
 
-
 let hiddenInput = document.getElementById("hidden-input")
-
-
-
 
 let best = localStorage.getItem("bestWPM");
 if (best) {
@@ -170,10 +166,7 @@ function calculateWpm() {
     
 }
 
-
-
-
-/*document.addEventListener("keydown", (e) => {
+hiddenInput.addEventListener("input", (e) => {ž
     if(!isTestActive) return;
 
     if(ignoredKeys.includes(e.key)) return;
@@ -230,7 +223,67 @@ function calculateWpm() {
     if(currentIndex < spans.length) {
         spans[currentIndex].classList.add("current")
     } 
-})    */
+}) 
+
+
+document.addEventListener("keydown", (e) => {
+    if(!isTestActive) return;
+
+    if(ignoredKeys.includes(e.key)) return;
+    
+    spans = text.querySelectorAll("span");
+    if(currentIndex === spans.length) {
+        isTestActive = false;
+        testSection.style.display = "none"
+        testCompleted.style.display = "block"
+        correct = currentIndex - incorrectLetters
+        let accuracy = correct / currentIndex * 100
+        let accurate = Math.round(accuracy)
+        let wpmTimed;
+        let wpmPassage;
+        document.getElementById("accuracy-final").innerText = `${accurate}%`
+        if(timeMode.value === "timed") {
+            let currentTimeTimed = 60 - timer
+            let minutesTimed = currentTimeTimed / 60
+            wpmTimed = correct / 5 / minutesTimed
+            document.getElementById("wpm-final").innerText = Math.round(wpmTimed) 
+        } else {
+            let currentTimePassage = timer / 60
+            wpmPassage = correct / 5 / currentTimePassage
+            document.getElementById("wpm-final").innerText = Math.round(wpmPassage)
+        }
+
+        let safeTimed = wpmTimed || 0
+        let safePassage = wpmPassage || 0
+        let finalWPM = Math.max(Math.round(safePassage), Math.round(safeTimed))
+        console.log(finalWPM, safePassage, Math.round(safeTimed))
+        let best = Number(localStorage.getItem("bestWPM")) || 0;
+        if(finalWPM > best) {
+            localStorage.setItem("bestWPM", finalWPM);
+            document.getElementById("best-wpm").innerText = finalWPM;
+        }
+        document.getElementById("all-char").textContent = spans.length
+        document.getElementById("incorrect-char").textContent = incorrectLetters
+        return;
+    }
+
+    let expected = text.querySelectorAll("span")[currentIndex].innerText
+
+    spans.forEach(span => span.classList.remove("current"))
+
+    if (e.key === expected) {  
+        spans[currentIndex].classList.add("correct")
+    } else {
+        spans[currentIndex].classList.add("incorrect")
+        incorrectLetters++;
+    }
+    
+    currentIndex++;
+
+    if(currentIndex < spans.length) {
+        spans[currentIndex].classList.add("current")
+    } 
+})    
 
 
 window.addEventListener("keydown", (e) =>  {
@@ -239,64 +292,4 @@ window.addEventListener("keydown", (e) =>  {
             e.preventDefault();
         }
     }
-})
-
-
-hiddenInput.addEventListener("keydown", (e) => {
-    if(!isTestActive) return;
-
-    if(ignoredKeys.includes(e.key)) return;
-    
-    spans = text.querySelectorAll("span");
-    if(currentIndex === spans.length) {
-        isTestActive = false;
-        testSection.style.display = "none"
-        testCompleted.style.display = "block"
-        correct = currentIndex - incorrectLetters
-        let accuracy = correct / currentIndex * 100
-        let accurate = Math.round(accuracy)
-        let wpmTimed;
-        let wpmPassage;
-        document.getElementById("accuracy-final").innerText = `${accurate}%`
-        if(timeMode.value === "timed") {
-            let currentTimeTimed = 60 - timer
-            let minutesTimed = currentTimeTimed / 60
-            wpmTimed = correct / 5 / minutesTimed
-            document.getElementById("wpm-final").innerText = Math.round(wpmTimed) 
-        } else {
-            let currentTimePassage = timer / 60
-            wpmPassage = correct / 5 / currentTimePassage
-            document.getElementById("wpm-final").innerText = Math.round(wpmPassage)
-        }
-
-        let safeTimed = wpmTimed || 0
-        let safePassage = wpmPassage || 0
-        let finalWPM = Math.max(Math.round(safePassage), Math.round(safeTimed))
-        console.log(finalWPM, safePassage, Math.round(safeTimed))
-        let best = Number(localStorage.getItem("bestWPM")) || 0;
-        if(finalWPM > best) {
-            localStorage.setItem("bestWPM", finalWPM);
-            document.getElementById("best-wpm").innerText = finalWPM;
-        }
-        document.getElementById("all-char").textContent = spans.length
-        document.getElementById("incorrect-char").textContent = incorrectLetters
-        return;
-    }
-
-    let expected = text.querySelectorAll("span")[currentIndex].innerText
-
-    spans.forEach(span => span.classList.remove("current"))
-
-    if (e.key === expected) {  
-        spans[currentIndex].classList.add("correct")
-    } else {
-        spans[currentIndex].classList.add("incorrect")
-        incorrectLetters++;
-    }
-    
-    currentIndex++;
-
-    if(currentIndex < spans.length) {
-        spans[currentIndex].classList.add("current")
-    } 
 })
